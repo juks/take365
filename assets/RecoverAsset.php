fb9b219bc1e2e8ee50563e370147029bd1b1cd66
Pavel Kornilov <lusever@lusever.com>
2016-01-16 01:52:43 +0300

    recover password fix

--- assets/RecoverAsset.php
+++ assets/RecoverAsset.php
@@ -8,18 +8,14 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class StoryAsset extends AssetBundle
+class RecoverAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/photoview.css',
-        'css/story.css',
     ];
     public $js = [
-        'js/plupload/plupload.full.js',
-        'js/photoview.js',
-        'js/story.js',
+        'js/recover.js',
     ];
     public $depends = [
         'app\assets\AppAsset',
