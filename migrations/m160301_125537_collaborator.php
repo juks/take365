f073870be028451622c6118099962c5dbb7adf17
Igor Askarov <ia@pay-me.ru>
2016-03-03 11:15:02 +0300

    work

--- migrations/m160301_125537_collaborator.php
+++ migrations/m160301_125537_collaborator.php
@@ -6,7 +6,7 @@ use yii\db\Migration;
 class m160301_125537_collaborator extends Migration
 {
     public function safeUp() {
-        $this->execute('CREATE TABLE story_collaborator (story_id int unsigned not null, user_id int unsigned not null, permission tinyint unsigned not null, is_confirmed tinyint unsigned not null, time_created int unsigned not null, index story_id(story_id), index user_id(user_id)) ENGINE innodb;');
+        $this->execute('CREATE TABLE story_collaborator (story_id int unsigned not null, user_id int unsigned not null, permission tinyint unsigned not null, is_confirmed tinyint unsigned not null, time_created int unsigned not null, index user_id(user_id), unique index uni (story_id, user_id, permission)) ENGINE innodb;');
         $this->execute('ALTER TABLE auth_user CHANGE banned is_banned tinyint unsigned not null;');
     }
 
