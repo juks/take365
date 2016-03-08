5eb88f32a53725cdaeed1e2fe42599c139ddad7e
Sergey Komarov <sergey.oracle@me.com>
2016-03-08 11:42:42 +0300

    Comments: second step

--- views/user-page/story.php
+++ views/user-page/story.php
@@ -27,12 +27,18 @@ $this->registerJs("initStory();");
     </select></td>
     </tr>
     </table>
-    <p><a href="#" id="delete-recover" class="small" onclick="Story.deleteRecover(<?= $story->id ?>); return false;"><?php if ($story->is_deleted): ?>Восстановить историю<?php else: ?>Удалить историю<?php endif ?></a></p>
+    <div class="story-info">
+      <a href="#" class="num-comments"><span class="fa fa-comment-o"></span> 13 комментариев</a>
+      <a href="#" id="delete-recover" onclick="Story.deleteRecover(<?= $story->id ?>); return false;"><?php if ($story->is_deleted): ?><span class="recover">Восстановить историю</span><?php else: ?><span class="delete">Удалить историю</span><?php endif ?></a>
+    </div>
   </form>
   <?php else: ?>
   <h1 class="story-title1"><?= $story->titleFilled ?></h1>
   <p class="story-desc"><?= $story->description_jvx ?></p>
-  <p>Автор истории —  <a href="<?= $user->url ?>"><?= $user->fullnameFilled ?></a></p>
+  <div class="story-info">
+    <p>Автор истории —  <a href="<?= $user->url ?>"><?= $user->fullnameFilled ?></a></p>
+    <p><a href="#" class="num-comments"><span class="fa fa-comment-o"></span> 13 комментариев</a></p>
+  </div>
   <?php endif ?>
   <div id="socialBlock" class="element">
   <?= $this->render('/blocks/socialBlock', ['fbAppId' => Helpers::getParam('components/authClientCollection/facebook/clientId'), 'pageUrl' => (\yii\helpers\Url::base(true) . \Yii::$app->request->url)]) ?>
@@ -61,17 +67,21 @@ $this->registerJs("initStory();");
 </div>
 <div class="comments">
   <h2 class="comments-title">Комментарии (269)</h2>
-  <div class="comment">
+  <div class="comment comment-my">
     <div class="comment-header">
       <div class="comment-user fa fa-user">
         <a href="#" class="comment-user-img" style="background-image: url(http://dev.take365.org/media/p1/userpic/8e/611/me.jpg);"></a>
       </div>
       <div class="comment-username"><a href="#">Бей лбом</a></div>
       <time class="comment-date">6 марта 2016, 15:33</time>
-      <div class="comment-url"><a href="#">#</a></div>
+      <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
+      <div class="comment-trash"><a href="#" title="Удалить комментарий" class="fa fa-trash-o"></a></div>
     </div>
     <div class="comment-text">
       И оно уже чувствуется по Prayers/Triangles... Не знаю, уловил ли ты, но лично я жадно вытянул из новой песни ароматы SNW. Пересечения есть, определенно. Хотя, опять же, в последнем интервью Чино сказал что этот трэк не характеризует весь альбом. Будем слушать... P.s. В DE и KNY действительно ногу сломишь, но я думаю первый больше Карпентеровский, а второй чистый сплав 50 на 50.
+    </div>
+    <div class="comment-options">
+      <a href="#" class="comment-options-item">Ответить</a>
     </div>
   </div>
   <div class="comments-reply">
@@ -82,25 +92,41 @@ $this->registerJs("initStory();");
         </div>
         <div class="comment-username"><a href="#">Lusever</a></div>
         <time class="comment-date">6 марта 2016, 16:13</time>
-        <div class="comment-url"><a href="#">#</a></div>
+        <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
       </div>
       <div class="comment-text">
         А мне Diamond Eyes дико зашёл после концерта, в живую треки из него прям вписались в общее представление о группе.
       </div>
+      <div class="comment-options">
+        <a href="#" class="comment-options-item">Ответить</a>
+      </div>
     </div>
     <div class="comments-reply">
-      <div class="comment">
+      <div class="comment comment-my">
         <div class="comment-header">
           <div class="comment-user fa fa-user">
             <a href="#" class="comment-user-img" style="background-image: url(http://dev.take365.org/media/p1/userpic/8e/611/me.jpg);"></a>
           </div>
           <div class="comment-username"><a href="#">Бей лбом</a></div>
           <time class="comment-date">6 марта 2016, 17:11</time>
-          <div class="comment-url"><a href="#">#</a></div>
+          <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
+          <div class="comment-trash"><a href="#" title="Удалить комментарий" class="fa fa-trash-o"></a></div>
         </div>
         <div class="comment-text">
           Doomed User уже на концертах играют, походу будет следующим синглом.
         </div>
+        <div class="comment-options" style="display: none;">
+          <a href="#" class="comment-options-item">Ответить</a>
+        </div>
+        <form action="/" class="form form-comment">
+          <fieldset>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </fieldset>
+          <fieldset>
+            <input type="submit" value="Отправить">
+            <a href="#" class="cancel">Отмена</a>
+          </fieldset>
+        </form>
       </div>
       <div class="comments-reply">
         <div class="comment">
@@ -110,10 +136,13 @@ $this->registerJs("initStory();");
             </div>
             <div class="comment-username"><a href="#">Lusever</a></div>
             <time class="comment-date">6 марта 2016, 17:47</time>
-            <div class="comment-url"><a href="#">#</a></div>
+            <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
           </div>
           <div class="comment-text">
             Что то очень интересное по моему... Такое неоднозначное, но притягательное...))
+          </div>
+          <div class="comment-options">
+            <a href="#" class="comment-options-item">Ответить</a>
           </div>
         </div>
         <div class="comments-reply">
@@ -124,10 +153,13 @@ $this->registerJs("initStory();");
               </div>
               <div class="comment-username"><a href="#">Oracle</a></div>
               <time class="comment-date">6 марта 2016, 17:51</time>
-              <div class="comment-url"><a href="#">#</a></div>
+              <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
             </div>
             <div class="comment-text">
               А у них всегда так, помню когда роузмери лайв только появился как все умилялись, а потом альбомная версия уже не торт.
+            </div>
+            <div class="comment-options">
+              <a href="#" class="comment-options-item">Ответить</a>
             </div>
           </div>
           <div class="comments-reply">
@@ -138,11 +170,24 @@ $this->registerJs("initStory();");
                 </div>
                 <div class="comment-username"><a href="#">Juks</a></div>
                 <time class="comment-date">6 марта 2016, 18:24</time>
-                <div class="comment-url"><a href="#">#</a></div>
+                <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
               </div>
               <div class="comment-text">
                 Слушай, а ведь реально так!
               </div>
+              <div class="comment-options" style="display: none;">
+                <a href="#" class="comment-options-item">Ответить</a>
+              </div>
+              <form action="/" class="form form-comment">
+                <fieldset class="error">
+                  <textarea name="" id="" cols="30" rows="10"></textarea>
+                  <span class="error-message">Зачем отправлять пустое сообщение, бро?</span>
+                </fieldset>
+                <fieldset>
+                  <input type="submit" value="Отправить">
+                  <a href="#" class="cancel">Отмена</a>
+                </fieldset>
+              </form>
             </div>
           </div>
         </div>
@@ -155,10 +200,13 @@ $this->registerJs("initStory();");
         </div>
         <div class="comment-username"><a href="#">Juks</a></div>
         <time class="comment-date">6 марта 2016, 16:52</time>
-        <div class="comment-url"><a href="#">#</a></div>
+        <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
       </div>
       <div class="comment-text">
         Ха, самым слабым считаю Diamond Eyes, ни одна песня не понравилась, второй плохиш Saturday Night Wrist. Самый крутой 1997 года, второй WP, третий Deftones. KNY - уже звучит как другая группа но понравился.
+      </div>
+      <div class="comment-options">
+        <a href="#" class="comment-options-item">Ответить</a>
       </div>
     </div>
     <div class="comment">
@@ -168,10 +216,13 @@ $this->registerJs("initStory();");
         </div>
         <div class="comment-username"><a href="#">Бей лбом</a></div>
         <time class="comment-date">6 марта 2016, 16:52</time>
-        <div class="comment-url"><a href="#">#</a></div>
+        <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
       </div>
       <div class="comment-text">
         Вкатил круче первого сингла, даже с таким звуком)
+      </div>
+      <div class="comment-options">
+        <a href="#" class="comment-options-item">Ответить</a>
       </div>
     </div>
   </div>
@@ -182,10 +233,13 @@ $this->registerJs("initStory();");
       </div>
       <div class="comment-username"><a href="#">Бей лбом</a></div>
       <time class="comment-date">6 марта 2016, 18:02</time>
-      <div class="comment-url"><a href="#">#</a></div>
+      <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
     </div>
     <div class="comment-text">
       Говнозвук конечно, наверное любимый трек Стефа с нового, дали оторваться.
+    </div>
+    <div class="comment-options">
+      <a href="#" class="comment-options-item">Ответить</a>
     </div>
   </div>
   <div class="comment">
@@ -195,10 +249,13 @@ $this->registerJs("initStory();");
       </div>
       <div class="comment-username"><a href="#">Lusever</a></div>
       <time class="comment-date">7 марта 2016, 02:17</time>
-      <div class="comment-url"><a href="#">#</a></div>
+      <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
     </div>
     <div class="comment-text">
       Они во время вайт пони и одноимённика вообще чуть не поубивали друг друга.
+    </div>
+    <div class="comment-options">
+      <a href="#" class="comment-options-item">Ответить</a>
     </div>
   </div>
   <div class="comment">
@@ -208,10 +265,32 @@ $this->registerJs("initStory();");
       </div>
       <div class="comment-username"><a href="#">Oracle</a></div>
       <time class="comment-date">7 марта 2016, 03:41</time>
-      <div class="comment-url"><a href="#">#</a></div>
+      <div class="comment-url"><a href="#" title="Ссылка на комментарий">#</a></div>
     </div>
     <div class="comment-text">
       А мне новая песнь больше всего ликнутый Чино Smile по настроению напомнила.
+    </div>
+    <div class="comment-options">
+      <a href="#" class="comment-options-item">Ответить</a>
+    </div>
+  </div>
+  <div class="comments-footer">
+    <form action="/" class="form form-comment">
+      <fieldset>
+        <legend>Ваш комментарий</legend>
+      </fieldset>
+      <fieldset>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </fieldset>
+      <fieldset>
+        <input type="submit" value="Отправить">
+        <a href="#" class="cancel">Отмена</a>
+      </fieldset>
+    </form>
+    <div class="comments-info">
+      <span class="fa fa-exclamation-triangle"></span>
+      Только зарегистрированные пользователи могут участвовать в обсуждениях.<br>
+      Если вы хотите оставить комментарий, пожалуйста, <a href="#">представьтесь</a> или <a href="#">зарегистрируйтесь</a>.
     </div>
   </div>
 </div>
