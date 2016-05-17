ec0a1cdd5ed2e35032faa6db8976682ef6fd5795
j <j@home.local>
2016-05-17 17:02:12 +0300

    https

--- views/blocks/socialBlock.php
+++ views/blocks/socialBlock.php
@@ -1,5 +1,5 @@
 <div class="fl" style="position: relative; top: 2px;">
     <iframe src="//www.facebook.com/plugins/like.php?app_id=<?= $fbAppId ?>&amp;href=<?= $pageUrl ?>&amp;send=false&amp;layout=button_count&amp;width=160&amp;show_faces=true&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:160px; height:21px;" allowTransparency="true"></iframe>
 </div>
-<div class="fl" style="position: relative; top: 2px;"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div>
+<div class="fl" style="position: relative; top: 2px;"><a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script></div>
 <div class="fl" style=""><g:plusone></g:plusone></div>
