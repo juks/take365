75ad9a8e3c66fe665451bb35b3218d594d9d1364
Sergey Komarov <skomarov@locomote.com>
2016-06-10 19:13:58 +0300

    Update markup

--- web/blocks/feed-item/feed-item.jsx
+++ web/blocks/feed-item/feed-item.jsx
@@ -33,24 +33,30 @@ export default class FeedItem extends React.Component {
   render() {
     const data = this.props.data;
     return <div className="feed-item">
-      <div className="feed-item-img">
+      <div className="feed-item-header">
         { data.story.authors
           .map(author => <a href={author.url} key={author.username}>{author.username}</a>)
         }
         &nbsp;→ <a href={data.story.url}>{data.story.title}</a>
       </div>
       <div className="feed-item-content">
-        <a href="" onClick={this.onLikeToggle}>{this.state.isLiked ? '♥' : '♡'}</a>
-        <a href={`${data.story.url}#${data.date}`}>
-          <img src={data.thumb.url} width={data.thumb.width} height={data.thumb.height} srcSet={`${data.thumbLarge.url} 2x`} />
-        </a>
+        <div className="feed-item-img">
+          <a href={`${data.story.url}#${data.date}`}>
+            <img src={data.thumb.url} width={data.thumb.width} height={data.thumb.height} srcSet={`${data.thumbLarge.url} 2x`} />
+          </a>
+        </div>
+        <div className="feed-item-desc">
+          { data.title ?
+            <p>{data.title}</p>
+          : null }
+          { data.description ?
+            <p dangerouslySetInnerHTML={{__html: data.description}}></p>
+          : null }
+        </div>
       </div>
-      { data.title ?
-        <p>{data.title}</p>
-      : null }
-      { data.description ?
-        <p dangerouslySetInnerHTML={{__html: data.description}}></p>
-      : null }
+      <div className="feed-item-footer">
+        <a href="" onClick={this.onLikeToggle}>{this.state.isLiked ? '♥' : '♡'}</a>
+      </div>
     </div>;
   }
 }
