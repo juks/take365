a7902867fb97aa35e198bc7e9aa1fbbbbc8cfbc4
Igor Askarov <juks@juks.ru>
2016-06-27 19:34:34 +0300

    deploy

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+set -e
 git pull
 ./yii cache/flush-all
 ./yii migrate/up
 composer install
 ./minify.sh
+echo "--------------------------------------------------------------"
+echo "-                                                            -"
+echo "-                                                            -"
+echo "-                          SUCCESS                           -"
+echo "-                                                            -"
+echo "-                                                            -"
+echo "--------------------------------------------------------------"
