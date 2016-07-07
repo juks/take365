915a3299ba51a2ecce2dfb91d9811452c44445e2
Igor Askarov <juks@juks.ru>
2016-07-07 18:39:14 +0300

    minify

--- minify.sh
+++ minify.sh
@@ -4,7 +4,8 @@ mkdir -p web/min/js
 mkdir -p web/min/css
 mkdir -p web/min-build/js
 mkdir -p web/min-build/css
-npm install && npm run build
+npm install
+npm run build
 ./yii asset config/minify.php config/assets_min.php
 find web/min/js/* -mtime +5 -exec rm {} \;
 find web/min/css/* -mtime +5 -exec rm {} \;
