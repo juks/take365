638ac2ad647d393d783063e6f6301c4a943df3fc
Igor Askarov <juks@juks.ru>
2016-09-13 17:04:25 +0300

    fix

--- views/user-page/story.php
+++ views/user-page/story.php
@@ -9,7 +9,7 @@ StoryAsset::register($this);
 $lastMonth = null;
 
 $reactComments =  \yii\helpers\Json::encode($story->comments);
-$reactUser =  \yii\helpers\Json::encode($user);
+$reactUser =  \yii\helpers\Json::encode($userd);
 
 $this->registerJs("initStory();appRender(document.getElementById('comments'),{comments:$reactComments,id:$story->id,user:$reactUser})");
 
