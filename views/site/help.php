d0de17da23db4302c7a6cdc73431571cb54671c3
Sergey Komarov <sergey.oracle@gmail.com>
2016-09-29 14:06:51 +0300

    Random stories block

--- views/site/help.php
+++ views/site/help.php
@@ -9,12 +9,71 @@
 <h2>Это вообще возможно?</h2>
 <p>Да, возможно! Сотни счастливчиков нашли способ организовать себя в&nbsp;непростом, но&nbsp;стоящем деле. Мы составили <a href="/howto/">несколько советов</a> &laquo;Как правильно подойти к&nbsp;делу и&nbsp;не&nbsp;разменяться на&nbsp;ерунду&raquo;.</p>
 <?php if($sampleStories): ?>
-<p>Вот несколько случайных примеров завершённых историй:</p>
-    <ul>
-    <?php foreach($sampleStories as $story): ?>
-        <li><a href="<?= $story->url ?>"><?= $story->titleFilled ?></a></li>
-    <?php endforeach ?>
-    </ul>
+<h3>Вот несколько случайных примеров завершённых историй</h3>
+<div class="story-random">
+  <div class="story">
+    <div class="story-content">
+      <div class="story-header">
+        <h4><a href="https://take365.org/Oracle/story/551">Хроники 2015</a></h4>
+      </div>
+      <div class="story-matrix">
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/db/15017/img_0046_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/7f/15016/img_0040_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/bd/15015/img_0041_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/2b/15014/img_0036_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/d8/15013/img_0035_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/f8/15012/img_0030_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/71/14923/img_0028_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/38/14922/img_0019_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/d3/14921/img_0018_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/551" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/d4/14920/img_0016_100x100.jpg" alt=""></a>
+      </div>
+    </div>
+  </div>
+  <div class="story">
+    <div class="story-content">
+      <div class="story-header">
+        <h4><a href="https://take365.org/Oracle/story/392">Хроники 2013—2014</a></h4>
+      </div>
+      <div class="story-matrix">
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/f1/12871/img_1151_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/1a/12663/img_1136_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/5a/12660/img_1132_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/d8/12661/img_1133_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/6e/12662/img_1134_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/20/12659/img_1125_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/09/12657/img_1123_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/8e/12656/img_1121_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/e4/12655/img_1115_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/392" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p2/storyImage/3b/12653/img_1112_100x100.jpg" alt=""></a>
+      </div>
+    </div>
+  </div>
+  <div class="story">
+    <div class="story-content">
+      <div class="story-header">
+        <h4><a href="https://take365.org/Oracle/story/197">Хроники 2012—2013</a></h4>
+      </div>
+      <div class="story-matrix">
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/c1/7703/dscf0428_-_2013-11-03_at_20-37-29_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/8f/7702/dscf0424_-_2013-11-03_at_20-29-14_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/35/7701/dscf0405_-_2013-11-03_at_12-29-03_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/1b/7685/img_0248_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/fb/7686/img_0243_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/60/7700/dscf0327_-_2013-04-11_at_15-22-57_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/6b/7677/dscf0313_-_2013-04-07_at_15-49-32_(2)_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/cf/7676/dscf0310_-_2013-03-25_at_02-29-11_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/35/7269/img_0227_100x100.jpg" alt=""></a>
+        <a href="https://take365.org/Oracle/story/197" class="story-matrix-item"><img src="https://take365.org/media/thumbs/p1/storyImage/69/7268/img_0226_100x100.jpg" alt=""></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- <ul>
+<?php foreach($sampleStories as $story): ?>
+    <li><a href="<?= $story->url ?>"><?= $story->titleFilled ?></a></li>
+<?php endforeach ?>
+</ul> -->
 <?php endif ?>
 <h2>Правила</h2>
 <p>Степень обязательности строго не&nbsp;определена и&nbsp;остаётся на&nbsp;усмотрение авторов историй. За&nbsp;то, когда была сделана фотография отвечают только они, они&nbsp;же решают, уместно&nbsp;ли заполнить день той фотографией, которая наиболее хорошо передаёт его настроение, но&nbsp;была сделана пару дней назад. Есть только два условия&nbsp;&mdash; авторство на&nbsp;все работы должно принадлежать автору истории и&nbsp;все фотографии должны быть сделаны во&nbsp;время этих самых 365-ти дней.</p>
