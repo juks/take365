3f4e9e5d8cca9be23b763050f12b421fe87f3213
Igor Askarov <juks@juks.ru>
2016-09-30 12:41:54 +0300

    empty story fix

--- models/Story.php
+++ models/Story.php
@@ -354,7 +354,7 @@ class Story extends StoryBase implements IPermissions, IGetType {
         }
 
         // Checking likes statistics
-        if (!$user->isGuest) {
+        if (!$user->isGuest && $imageIds) {
             $likes = Like::find()->where(Like::makeCondition(['target_id' => ['IN', $imageIds], 'target_type' => Media::typeId, 'created_by' => $user->id, 'is_active' => 1]))->all();
             if ($likes) $likesHash = Helpers::makeDict($likes, 'target_id');
         }
