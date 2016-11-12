eb8e9133a9d92510a76bc84af5040678d36bb1f4
Igor Askarov <juks@juks.ru>
2016-11-13 01:48:56 +0300

    validation fix

--- messages/ru/story.php
+++ messages/ru/story.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'Time difference exceeds limit' => 'Временная разница превосходит допустимый предел',
+    'Given date is not valid for this story' => 'Указанная дата неприменима к этой истории'
 ];
