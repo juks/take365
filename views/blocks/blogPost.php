e497b6702a7f747652e8d6b4f900a1d8770dbcbf
Sergey Komarov <sergey.oracle@gmail.com>
2016-11-15 11:58:29 +0300

    UI for blog

--- views/blocks/blogPost.php
+++ views/blocks/blogPost.php
@@ -1,5 +1,12 @@
-
-
-<div><?= $post->title ?></div>
-<div><?= $post->body_jvx ?></div>
-<div><a href="<?= $post->author->url ?>"><?= $post->author->username ?></a></div>
+<section class="content blog">
+  <article class="article-blog article-blog-full">
+    <header class="article-header">
+      <!-- <time datetime="2016-09-30 20:00">30.09.2016</time> -->
+      <h2><?= $post->title ?></h2>
+    </header>
+    <?= $post->body_jvx ?>
+    <footer class="article-footer">
+      <a href="<?= $post->author->url ?>"><?= $post->author->username ?></a>
+    </footer>
+  </article>
+</section>
--- views/panel/Post.php
+++ views/panel/Post.php
@@ -6,24 +6,30 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Post */
 /* @var $form ActiveForm */
+
 ?>
-<div class="Post">
 
-    <?php
-        $form = ActiveForm::begin();
-        $model->blog_id = 1;
-        $model->is_published = 1;
-    ?>
+<div class="blog-post">
+  <?php
+    $form = ActiveForm::begin(
+      [
+        'options' => [
+          'class' => 'form'
+        ]
+      ]
+    );
+    $model->blog_id = 1;
+    $model->is_published = 1;
+  ?>
 
-        <?= $form->field($model, 'id')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'blog_id')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'is_published')->hiddenInput()->label(false) ?>
-        <?= $form->field($model, 'title') ?>
-        <?= $form->field($model, 'body')->textArea(['rows' => '6']) ?>
+    <?= $form->field($model, 'id')->hiddenInput()->label(false) ?>
+    <?= $form->field($model, 'blog_id')->hiddenInput()->label(false) ?>
+    <?= $form->field($model, 'is_published')->hiddenInput()->label(false) ?>
+    <?= $form->field($model, 'title') ?>
+    <?= $form->field($model, 'body')->textArea(['rows' => '6']) ?>
 
-        <div class="form-group">
-            <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
-        </div>
-    <?php ActiveForm::end(); ?>
-
-</div><!-- Post -->
+    <fieldset>
+      <?= Html::submitInput(Yii::t('app', 'Submit')) ?>
+    </fieldset>
+  <?php ActiveForm::end(); ?>
+</div>
