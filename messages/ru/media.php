eab706174f2ff358c944029d56a3bd27e8838370
Igor Askarov <juks@juks.ru>
2016-11-28 13:17:23 +0300

    work

--- messages/ru/media.php
+++ messages/ru/media.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'Media storage quota not set' => 'Не заданы настроки ограничений хранения media',
-    'Failed to detect date'       => 'Не удалось определить дату'
+    'Failed to detect date'       => 'Не удалось определить дату',
+    'Unsupported file type'       => 'Данный тип файлов не поддерживается'
 ];
