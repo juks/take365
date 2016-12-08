c3feadc973b599352b6abe47d7ebb3b9ba8bdd5a
Sergey Komarov <sergey.oracle@gmail.com>
2016-12-08 17:30:51 +0300

    Fix for unsubscribe

--- views/site/unsubscribe.php
+++ views/site/unsubscribe.php
@@ -1,6 +1,13 @@
-<?php if ($actionResult): ?>
-    <p><?= $actionResult ?></p>
-<?php endif;?>
-<p>
-    <a href="<?= $actionUrl ?>"><?= $actionTitle ?></a>
-</p>
+<main class="content">
+  <section class="content-info">
+    <header class="content-header">
+			<h1 class="content-title">Настройка уведомлений</h1>
+    </header>
+    <?php if ($actionResult): ?>
+		  <p><?= $actionResult ?></p>
+		<?php endif;?>
+		<p>
+		  <a href="<?= $actionUrl ?>"><?= $actionTitle ?></a>
+		</p>
+  </section>
+</main>
