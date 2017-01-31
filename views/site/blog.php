942fb5092d963daf5bdca60f1b9e4541d8f320fa
Pavel Kornilov <lusever@lusever.com>
2017-01-31 18:54:17 +0300

    add main block to blog

--- views/site/blog.php
+++ views/site/blog.php
@@ -5,9 +5,11 @@
 ?>
 
 <?php if ($posts): ?>
-    <?php foreach($posts as $post): ?>
-        <?= $this->render('//blocks/blogPost', ['post' => $post]); ?>
-    <?php endforeach ?>
+  <main class="content blog">
+      <?php foreach($posts as $post): ?>
+          <?= $this->render('//blocks/blogPost', ['post' => $post]); ?>
+      <?php endforeach ?>
+  </main>
 <?php else: ?>
 
 <?php endif ?>
