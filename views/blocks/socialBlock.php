dad8d820f67c7e284e7c74ad54520769c0026843
Sergey Komarov <sergey.oracle@gmail.com>
2017-02-10 12:35:03 +0300

    Refactoring

--- views/blocks/socialBlock.php
+++ views/blocks/socialBlock.php
@@ -1,5 +1,9 @@
-<div class="fl" style="position: relative; top: 2px;">
-    <iframe src="//www.facebook.com/plugins/like.php?app_id=<?= $fbAppId ?>&amp;href=<?= $pageUrl ?>&amp;send=false&amp;layout=button_count&amp;width=160&amp;show_faces=true&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:160px; height:21px;" allowTransparency="true"></iframe>
+<div class="social-item">
+  <a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
 </div>
-<div class="fl" style="position: relative; top: 2px;"><a href="https://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script></div>
-<div class="fl" style=""><g:plusone></g:plusone></div>
+<div class="social-item">
+  <g:plusone></g:plusone>
+</div>
+<div class="social-item">
+  <iframe src="//www.facebook.com/plugins/like.php?app_id=<?= $fbAppId ?>&amp;href=<?= $pageUrl ?>&amp;send=false&amp;layout=button_count&amp;width=160&amp;show_faces=true&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:160px; height:21px;" allowTransparency="true"></iframe>
+</div>
--- views/user-page/story.php
+++ views/user-page/story.php
@@ -15,72 +15,101 @@ $this->registerJs("initStory();appRender(document.getElementById('comments'),{co
 
 ?>
 
-<main class="content stories">
-  <div id="userPhotos">
+<main class="content stories" id="userPhotos">
+  <header class="content-header">
     <?php if ($story): ?>
     <?php if ($canManage): ?>
-    <form id="storyEditForm" class="editable-not-editing form" action="/api/story/write" method="post" name="storyEditForm">
-      <input name="id" type="hidden" value="<?= $story->id ?>">
-      <h1 class="story-title editable"><span class="editable-placeholder<?php if ($story->title): ?> hidden<?php endif ?>">Кликните, чтобы добавить заголовок</span><span class="editable-text"><?= $story->title ?></span><input class="editable-input" placeholder="Кликните, чтобы добавить заголовок" type="text" maxlength="255" value="<?= $story->title ?>" name="title"></h1>
-      <p class="story-desc editable"><span class="editable-placeholder<?php if ($story->description): ?> hidden<?php endif ?>">Кликните, чтобы добавить описание</span><span class="editable-text"><?= $story->description ?></span><textarea class="editable-input" placeholder="Кликните, чтобы добавить описание" name="description"><?= $story->description ?></textarea></p>
-      <p class="story-submit"><input value="Сохранить" type="submit" class="editable-submit"> <input value="Отмена" type="reset" class="editable-cancel"></p>
-      <table id="settingsHolder">
-      <tr id="statusSelectorHoder"<?php if ($story->is_deleted): ?> class="hidden"<?php endif ?>>
-      <td>Статус</td>
-      <td><select name="status" id="storyStatusSelector" onchange="Story.updateStatus(pp.storyId); return false;">
-        <option value="<?= Story::statusHidden ?>"<?php if ($story->status == Story::statusHidden): ?> selected<?php endif ?>>Скрыта</option>
-        <option value="<?= Story::statusPublic ?>"<?php if ($story->status == Story::statusPublic): ?> selected<?php endif ?>>В публичном доступе</option>
-      </select></td>
-      </tr>
-      </table>
-      <div class="story-info">
-        <a href="#comments" class="num-comments"><?= Ml::t('{n,plural,=0{No comments} =1{One Comment} other{# Comments}}', null, ['n' => $story->comments_count ]) ?></a>
-        <a href="#" id="delete-recover" onclick="Story.deleteRecover(<?= $story->id ?>); return false;"><?php if ($story->is_deleted): ?><span class="recover">Восстановить историю</span><?php else: ?><span class="delete">Удалить историю</span><?php endif ?></a>
-      </div>
-    </form>
+      <form id="storyEditForm" class="editable-not-editing form" action="/api/story/write" method="post" name="storyEditForm">
+        <input name="id" type="hidden" value="<?= $story->id ?>">
+        <h1 class="story-title editable">
+          <span class="editable-placeholder<?php if ($story->title): ?> hidden<?php endif ?>">Кликните, чтобы добавить заголовок</span>
+          <span class="editable-text"><?= $story->title ?></span>
+          <input class="editable-input" placeholder="Кликните, чтобы добавить заголовок" type="text" maxlength="255" value="<?= $story->title ?>" name="title">
+        </h1>
+        <div class="story-desc editable">
+          <span class="editable-placeholder<?php if ($story->description): ?> hidden<?php endif ?>">Кликните, чтобы добавить описание</span>
+          <span class="editable-text"><?= $story->description ?></span>
+          <textarea class="editable-input" placeholder="Кликните, чтобы добавить описание" name="description"><?= $story->description ?></textarea>
+        </div>
+        <div class="story-submit">
+          <input value="Сохранить" type="submit" class="editable-submit">
+          <input value="Отмена" type="reset" class="editable-cancel">
+        </div>
+        <div class="story-settings<?php if ($story->is_deleted): ?> hidden<?php endif ?>" id="statusSelectorHoder">
+          <label>Статус:</label>
+          <select name="status" id="storyStatusSelector" onchange="Story.updateStatus(pp.storyId); return false;">
+            <option value="<?= Story::statusHidden ?>"<?php if ($story->status == Story::statusHidden): ?> selected<?php endif ?>>Скрыта</option>
+            <option value="<?= Story::statusPublic ?>"<?php if ($story->status == Story::statusPublic): ?> selected<?php endif ?>>В публичном доступе</option>
+          </select>
+        </div>
+        <div class="story-footer">
+          <a href="#comments" class="num-comments"><?= Ml::t('{n,plural,=0{No comments} =1{One Comment} other{# Comments}}', null, ['n' => $story->comments_count ]) ?></a>
+          <span class="sep">·</span>
+          <a href="#" class="delete-recover" id="delete-recover" onclick="Story.deleteRecover(<?= $story->id ?>); return false;"><?php if ($story->is_deleted): ?><span class="recover">Восстановить историю</span><?php else: ?><span class="delete">Удалить историю</span><?php endif ?></a>
+        </div>
+      </form>
+
     <?php else: ?>
-    <h1 class="story-title"><?= $story->titleFilled ?></h1>
-    <p class="story-desc"><?= $story->description_jvx ?></p>
-    <div class="story-info">
-      <p>Автор истории —  <a href="<?= $owner->url ?>"><?= $owner->fullnameFilled ?></a></p>
-      <p><a href="#comments" class="num-comments"><?= Ml::t('{n,plural,=0{No comments} =1{One Comment} other{# Comments}}', null, ['n' => $story->comments_count ]) ?></a></p>
-    </div>
-    <?php endif ?>
-    <div id="socialBlock" class="element">
-    <?= $this->render('/blocks/socialBlock', ['fbAppId' => Helpers::getParam('components/authClientCollection/facebook/clientId'), 'pageUrl' => (\yii\helpers\Url::base(true) . \Yii::$app->request->url)]) ?>
-    </div>
-    <div class="cl"></div>
-    <div class="yearTitle"><?= $story->yearStart ?>—<?= $story->yearEnd ?></div>
-    <?php foreach ($story->calendar as $day): ?>
-    <?php if ($lastMonth != $day['monthTitle']): ?><p class="hugeTitle tbo story-mounth cl"><?= $day['monthTitle'] ?></p><?php endif ?>
-      <div <?php if (!empty($day['id'])): ?> data-id="<?= $day['id'] ?>" <?php endif ?>id="day-<?= $day['date'] ?>" class="user-photo <?= !empty($day['isUploadable']) ? 'available' : 'dummy fa fa-clock-o' ?><?php if (!empty($day['isEmpty'])): ?> empty i-upload<?php endif ?>">
-        <div class="user-photo-day"><?= $day['monthDay'] ?></div>
-        <?php if (empty($day['isEmpty'])): ?>
-          <?php if (empty($day['invisible'])): ?>
-          <div class="user-photo-content">
-            <a href="<?= $day['url'] ?>"><img src="<?= $day['image']['url'] ?>" width="<?= $day['image']['width'] ?>" height="<?= $day['image']['height'] ?>" class="user-photo-image"></a>
-            <?php if ($canUpload): ?><div class="user-photo-manage">Редактировать</div><?php endif ?>
-            <?php if (!empty($day['isDeleted'])): ?><div class="user-photo-restore"><a class="ctrl-restore" onclick="Story.recoverMedia('<?= $day['date'] ?>')">Восстановить</a> или <a class="ctrl-replace i-upload" onclick="Story.openUpload('<?= $day['date'] ?>')">заменить</a>.</div><?php endif ?>
-              <?php if ($user): ?>
-                <div class="user-photo-likes">
-                <a href="#" class="fa fa-heart<?= $day['isLiked'] ? '' : '-o' ?> user-photo-like"></a>
-                <span class="user-photo-like-total"><?= $day['likesCount'] ?: '' ?></span>
-                </div>
-              <?php elseif (!empty($day['likesCount'])): ?>
-                <div class="user-photo-likes">
-                <span class="fa fa-heart user-photo-like"></span>
-                <span class="user-photo-like-total"><?= $day['likesCount'] ?: '' ?></span>
-                </div>
-              <?php endif ?>
-          </div>
-          <?php endif ?>
+      <h1 class="story-title"><?= $story->titleFilled ?></h1>
+      <div class="story-desc">
+        <p>Автор истории —  <a href="<?= $owner->url ?>"><?= $owner->fullnameFilled ?></a></p>
+        <?php if ($story->description_jvx): ?>
+          <p><?= $story->description_jvx ?></p>
         <?php endif ?>
       </div>
-    <?php
-      $lastMonth = $day['monthTitle'];
-    ?>
-    <?php endforeach ?>
+      <div class="story-footer">
+        <a href="#comments" class="num-comments"><?= Ml::t('{n,plural,=0{No comments} =1{One Comment} other{# Comments}}', null, ['n' => $story->comments_count ]) ?></a>
+      </div>
+
     <?php endif ?>
-  </div>
+      <div class="social">
+        <?= $this->render('/blocks/socialBlock', ['fbAppId' => Helpers::getParam('components/authClientCollection/facebook/clientId'), 'pageUrl' => (\yii\helpers\Url::base(true) . \Yii::$app->request->url)]) ?>
+      </div>
+
+    <?php endif ?>
+  </header>
+
+  <?php if ($story): ?>
+    <h2><?= $story->yearStart ?>—<?= $story->yearEnd ?></h2>
+
+    <?php foreach ($story->calendar as $day): ?>
+      <section class="story-month">
+        <?php if ($lastMonth != $day['monthTitle']): ?>
+          <h3><?= $day['monthTitle'] ?></h3>
+        <?php endif ?>
+
+        <ul class="story-list">
+          <li <?php if (!empty($day['id'])): ?> data-id="<?= $day['id'] ?>" <?php endif ?>id="day-<?= $day['date'] ?>" class="story-item user-photo <?= !empty($day['isUploadable']) ? 'available' : 'dummy fa fa-clock-o' ?><?php if (!empty($day['isEmpty'])): ?> empty i-upload<?php endif ?>">
+            <div class="user-photo-day"><?= $day['monthDay'] ?></div>
+            <?php if (empty($day['isEmpty'])): ?>
+              <?php if (empty($day['invisible'])): ?>
+              <div class="user-photo-content">
+                <a href="<?= $day['url'] ?>"><img src="<?= $day['image']['url'] ?>" width="<?= $day['image']['width'] ?>" height="<?= $day['image']['height'] ?>" class="user-photo-image"></a>
+                <?php if ($canUpload): ?><div class="user-photo-manage">Редактировать</div><?php endif ?>
+                <?php if (!empty($day['isDeleted'])): ?><div class="user-photo-restore"><a class="ctrl-restore" onclick="Story.recoverMedia('<?= $day['date'] ?>')">Восстановить</a> или <a class="ctrl-replace i-upload" onclick="Story.openUpload('<?= $day['date'] ?>')">заменить</a>.</div><?php endif ?>
+                  <?php if ($user): ?>
+                    <div class="user-photo-likes">
+                    <a href="#" class="fa fa-heart<?= $day['isLiked'] ? '' : '-o' ?> user-photo-like"></a>
+                    <span class="user-photo-like-total"><?= $day['likesCount'] ?: '' ?></span>
+                    </div>
+                  <?php elseif (!empty($day['likesCount'])): ?>
+                    <div class="user-photo-likes">
+                    <span class="fa fa-heart user-photo-like"></span>
+                    <span class="user-photo-like-total"><?= $day['likesCount'] ?: '' ?></span>
+                    </div>
+                  <?php endif ?>
+              </div>
+              <?php endif ?>
+            <?php endif ?>
+          </li>
+        </ul>
+
+        <?php
+          $lastMonth = $day['monthTitle'];
+        ?>
+      </section>
+    <?php endforeach ?>
+  <?php endif ?>
+
   <div class="comments" id="comments"></div>
 </main>
