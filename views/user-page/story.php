43bb9330c89e9b9840ab91a50fd2cc04a3c76198
Pavel Kornilov <lusever@lusever.com>
2017-02-16 19:11:02 +0300

    story edit link

--- views/user-page/story.php
+++ views/user-page/story.php
@@ -45,7 +45,7 @@ $this->registerJs("initStory();appRender(document.getElementById('comments'),{co
         <div class="story-footer">
           <a href="#comments" class="num-comments"><?= Ml::t('{n,plural,=0{No comments} =1{One Comment} other{# Comments}}', null, ['n' => $story->comments_count ]) ?></a>
           <span class="sep">·</span>
-          <a href="#" class="edit">Редактировать историю</a>
+          <a href="#" class="edit" onClick="$('#storyEditForm').toggleClass('editable-editing editable-not-editing')">Редактировать историю</a>
           <span class="sep">·</span>
           <a href="#" class="delete-recover" id="delete-recover" onclick="Story.deleteRecover(<?= $story->id ?>); return false;"><?php if ($story->is_deleted): ?><span class="recover">Восстановить историю</span><?php else: ?><span class="delete">Удалить историю</span><?php endif ?></a>
         </div>
